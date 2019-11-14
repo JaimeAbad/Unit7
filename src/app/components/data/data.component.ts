07a5db7ab92961cb6bb7c45fcf82a1b964636f28
Jaime <aquileseeb@gmail.com>
2019-11-14 09:26:28 +0100

    c3
    diapo28

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-data',
@@ -6,8 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./data.component.css']
 })
 export class DataComponent implements OnInit {
+  forma: FormGroup;
 
-  constructor() { }
+  usuario: Object={
+    nombreCompleto: {
+      nombre: "Carmen",
+      apellido: "Baena"
+    },
+    correo: "carmen@hotmail.com"
+  }
+
+
+  constructor() {
+    this.forma = new FormGroup({
+      'nombreCompleto': new FormGroup({
+        'nombre': new FormControl('', [
+          Validators.required,
+          Validators.minLength(3)
+        ]),
+        'apellido': new FormControl('', [
+          Validators.required
+        ])
+      }),
+      'correo': new FormControl('', [
+        Validators.required,
+        Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")
+      ])
+    });
+  }
 
   ngOnInit() {
   }
